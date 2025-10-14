b91f269645bab73a92c18f28879c840a42968246
puridanish10 <puri.danish10@gmail.com>
2025-10-14 10:35:31 +0530

    Create ques3

---
+++ assignment/lab_Assignment5/ques3.cpp
@@ -0,0 +1,125 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+public:
+     int data;
+     Node* next;
+     Node(int value){
+          data = value;
+          next = NULL;
+     }
+};
+class List{
+     Node* head;
+     Node* tail;
+public:
+     List(){
+          head = tail = NULL;
+     }
+     void insert(int value, int pos){
+          if(pos<0){ cout << "Invalid Position" <<endl;
+               return;
+          }if(pos == 0){
+               push_front(value);
+               return;
+          }
+          Node* temp = head;
+          for (int i=0; i<pos-1;i++){
+               temp = temp->next;
+          }
+          Node* newNode = new Node(value);
+          newNode->next = temp->next; // Now newNode replace the posn of temp
+          temp->next =newNode;
+     }
+
+     /// Implementing Search functionality in LL
+     int search(int key){
+          Node* temp = head;
+          int idx = 0;
+          while(temp!=NULL){
+               if(temp->data == key){
+                    return idx;
+               }
+               temp = temp -> next;
+               idx++;
+
+          }
+          return -1;
+
+     }
+
+
+
+
+     void push_front(int value){
+          Node* newNode = new Node(value);
+          if(head == NULL){// when LL is empty
+               head = tail = newNode;
+               return;
+          }else{// when LL is not empty
+               newNode->next = head;
+               head = newNode;
+          }
+     }
+     void push_back(int value){
+          Node* newNode = new Node(value);
+          if(head == NULL){
+               head = tail = newNode;
+          }else{// when LL is not empty
+               tail->next = newNode;
+               tail = newNode;
+          }
+     
+     }
+
+
+     void printLL(){
+          Node* temp = head;
+          while(temp != NULL){
+               cout<< temp->data <<" -> " ;
+               temp = temp -> next;
+          }
+          cout<<"NULL"<<endl;
+     }
+     void pop_front(){
+          if(head==NULL){
+               cout<<"LL is Empty"<<endl;
+               return;
+          }
+          Node* temp = head;// giving current head value to tmep
+          head = head->next;// pushing head to claim the next value
+          temp->next = NULL;//Temp value ie old head is pointing to NULL address
+          
+          delete temp;
+     }
+
+     void pop_back(){
+          if(head == NULL){
+               cout<<"LL is Empty" << endl;
+               return;
+          }
+          Node* temp = head;
+          while(temp->next != tail ){
+               temp = temp->next;
+          }
+          
+          // Now since Temp is finally the next one
+          temp->next = NULL;
+          delete tail;
+          tail = temp;
+     }
+};
+
+int main(){
+     List ll; //  declaring a list
+     ll.push_front(3);
+     ll.push_front(2);
+     ll.push_front(1);
+     
+     ll.insert(4,1);
+     cout<<ll.search(1)<<endl;
+
+     ll.printLL();
+     return 0;
+}
