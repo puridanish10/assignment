b3bdbfb310d73f9949a3516925be166fa27f33c1
puridanish10 <puri.danish10@gmail.com>
2025-12-02 22:34:35 +0530

    Add TreeNode class and InPreToBST function

---
+++ assignment/lab_Assignment8/ques7.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class TreeNode{
+public:
+     int val;
+     TreeNode* left;
+     TreeNode* right;
+     TreeNode(int value){
+          val = value;
+          left = right = nullptr;
+     }
+};
+
+void InPreToBST(TreeNode* root){
+
+     
+}
